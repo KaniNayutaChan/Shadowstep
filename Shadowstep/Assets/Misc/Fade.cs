1ec36704ff76ac4e4ebeb474cf6e6c910dff3869
Joshua Tan <jaykid1jaykid1@gmail.com>
2021-06-27 23:49:46 +0930

    changed transition to fade

--- Shadowstep/Assets/Misc/Fade.cs
+++ Shadowstep/Assets/Misc/Fade.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Transition : MonoBehaviour
+public class Fade : MonoBehaviour
 {
     Image image;
     bool isFadingIn = true;
